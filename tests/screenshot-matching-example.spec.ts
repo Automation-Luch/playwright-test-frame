9c652c43eaeaff07eef8f0efad102f9de4601772
Automation-Luch <lopezmichaela1b031@gmail.com>
2021-06-16 17:32:03 +0300

    screenshot tests example

--- tests/screenshot-matching-example.spec.ts
+++ tests/screenshot-matching-example.spec.ts
@@ -3,7 +3,11 @@ import { expect, test } from '@playwright/test';
 import { screenshotMatching, navigate } from '../lib/helpers/helpers';
 import { alty_cmd_stage } from '../lib/helpers/credentials';
 
-test('Check Snapshot making', async ({ browserName, page }) => {
+test('Check Snapshot making', async ({ page }, testInfo) => {
   await navigate(alty_cmd_stage.URL, page);
-  await screenshotMatching(browserName, expect, page);
+  await screenshotMatching(testInfo.title, expect, page, 0.7);
+});
+test('Check Snapshot making second', async ({ page }, testInfo) => {
+  await navigate(alty_cmd_stage.URL, page);
+  await screenshotMatching(testInfo.title, expect, page, 0.7);
 });
