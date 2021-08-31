dfdaaf4e70984364e42db0d01cdf1d5092c50025
Automation-Luch <lopezmichaela1b031@gmail.com>
2021-08-31 11:15:16 +0300

    fixture refactoring

--- tests/authentication.spec.ts
+++ tests/authentication.spec.ts
@@ -1,4 +1,6 @@
-import { test, expect } from '@playwright/test';
+import { expect } from '@playwright/test';
+import { test } from '../lib/fixtures/objects-as-default-fixtere-to-test';
+
 import { click, getText, navigate, emailCreate } from '../lib/helpers/helpers';
 import { errors } from '../lib/helpers/text-helpers';
 import { Login } from '../lib/page-objects/authentication';
@@ -9,54 +11,50 @@ import { FORM, NAVBAR, ERROR } from '../lib/selectors/base';
 import { alty_cmd_stage } from '../lib/helpers/credentials';
 
 const email = emailCreate();
-let auth;
-let dashboard;
 
 test.describe('Alty CMD Authentication', () => {
   // Run tests in this describe block with special timeout and viewport.
   test.use({ timeout: 50000, viewport: { width: 1920, height: 1080 } }); //
   test.beforeEach(async ({ page }) => {
-    auth = new Login(page);
-    dashboard = new AltyCMD(page);
     await navigate(alty_cmd_stage.URL, page);
   });
 
-  test('Check the ability to registration with option fields', async ({}) => {
+  test('Check the ability to registration with option fields', async ({ auth }) => {
     await auth.registration(email, alty_cmd_stage.PASSWORD);
     await auth.confirmation(email, 0);
   });
 
-  test('Check the ability to registration without option fields', async ({}) => {
+  test('Check the ability to registration without option fields', async ({ auth }) => {
     await auth.registration(email, alty_cmd_stage.PASSWORD, false);
     await auth.confirmation(email, 0);
   });
 
-  test('Check the ability login and logout ', async ({ page }) => {
+  test('Check the ability login and logout ', async ({ auth, page }) => {
     await auth.login(email, alty_cmd_stage.PASSWORD);
     const navbar = await getText(NAVBAR, page);
     expect(navbar).toContain('My Balance:');
   });
 
-  test('Check the ability to restore password', async ({ page }) => {
+  test('Check the ability to restore password', async ({ auth, page }) => {
     await auth.restorePassword(alty_cmd_stage.EMAIL_FOR_RESTORE, alty_cmd_stage.PASSWORD);
     const resetPage = await getText(FORM, page);
     expect(resetPage).toContain('Your password has been reset.');
   });
 
   test.describe('Alty CMD authentication NEGATIVE tests', () => {
-    test('Check the ability login with invalid (non-registered) email ', async ({ page }) => {
+    test('Check the ability login with invalid (non-registered) email ', async ({ auth, page }) => {
       await auth.login('email@mail.com', alty_cmd_stage.PASSWORD);
       const loginError = await getText(ERROR, page);
       expect(loginError).toContain(errors.INVALID_CREDENTIALS);
     });
 
-    test('Check the ability login with valid email and invalid password ', async ({ page }) => {
+    test('Check the ability login with valid email and invalid password ', async ({ auth, page }) => {
       await auth.login(alty_cmd_stage.EMAIL_FOR_RESTORE, 'ggpp123SS');
       const loginError = await getText(ERROR, page);
       expect(loginError).toContain(errors.INVALID_CREDENTIALS);
     });
 
-    test('Check the ability registration with invalid email address ', async ({ page }) => {
+    test('Check the ability registration with invalid email address ', async ({ auth, page }) => {
       await auth.registration('email', alty_cmd_stage.PASSWORD);
       const regError = await getText(ERROR, page);
       expect(regError).toContain(errors.INVALID_EMAIL);
@@ -67,27 +65,27 @@ test.describe('Alty CMD Authentication', () => {
         expect(errorText).toContain('This field is required.');
       });
 
-      test('Check the ability login with empty form ', async ({}) => {
+      test('Check the ability login with empty form ', async ({ auth }) => {
         await auth.login('', '');
       });
 
-      test('Check the ability login with empty email field  ', async ({}) => {
+      test('Check the ability login with empty email field  ', async ({ auth }) => {
         await auth.login('', alty_cmd_stage.PASSWORD);
       });
 
-      test('Check the ability login with empty password  ', async ({}) => {
+      test('Check the ability login with empty password  ', async ({ auth }) => {
         await auth.login(alty_cmd_stage.EMAIL_FOR_RESTORE, '');
       });
 
-      test('Check the ability to registration without email and password', async ({}) => {
+      test('Check the ability to registration without email and password', async ({ auth }) => {
         await auth.registration('', '');
       });
 
-      test('Check the ability to registration without password ', async ({}) => {
+      test('Check the ability to registration without password ', async ({ auth }) => {
         await auth.registration('email@mail.com', '');
       });
 
-      test('Check the ability to registration without email ', async ({}) => {
+      test('Check the ability to registration without email ', async ({ auth }) => {
         await auth.registration('', alty_cmd_stage.PASSWORD);
       });
     });
